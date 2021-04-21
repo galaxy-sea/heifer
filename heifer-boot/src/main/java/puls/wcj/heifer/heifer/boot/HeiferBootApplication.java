ffef9cd2e5f36e36ac8f822e77c7eb27634a49df
galaxy <469753862@qq.com>
2021-04-21 13:57:14 +0800

    初始化项目

---
+++ heifer-boot/src/main/java/puls/wcj/heifer/heifer/boot/HeiferBootApplication.java
@@ -0,0 +1,13 @@
+package puls.wcj.heifer.heifer.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiferBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeiferBootApplication.class, args);
+    }
+
+}
---
+++ heifer-boot/src/test/java/puls/wcj/heifer/heifer/boot/HeiferBootApplicationTests.java
@@ -0,0 +1,13 @@
+package puls.wcj.heifer.heifer.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiferBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
