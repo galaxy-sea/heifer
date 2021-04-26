b0acb81538d875efaa4a9757e42b76883f7b39e1
galaxy <469753862@qq.com>
2021-04-26 09:26:17 +0800

    优化配置

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
@@ -1,9 +1,9 @@
 package plus.wcj.heifer.boot.common.mvc;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import plus.wcj.heifer.boot.common.mvc.resolver.tenant.TenantMethodArgumentResolver;
@@ -14,7 +14,7 @@ import java.util.List;
  * @author changjin wei(魏昌进)
  * @date 2021/4/23
  */
-@Configuration
+@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
 
