b13f8b21712cb4192edf47a721bb6dba95741e7f
galaxy <469753862@qq.com>
2021-04-28 10:12:05 +0800

    精简项目

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
@@ -30,12 +30,4 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         converters.removeIf(httpMessageConverter -> StringHttpMessageConverter.class.equals(httpMessageConverter.getClass()));
     }
 
-
-    // @Override
-    // public void addViewControllers(ViewControllerRegistry registry) {
-    //     registry.addViewController("/home").setViewName("home");
-    //     registry.addViewController("/").setViewName("home");
-    //     registry.addViewController("/hello").setViewName("hello");
-    //     registry.addViewController("/login").setViewName("login");
-    // }
 }
