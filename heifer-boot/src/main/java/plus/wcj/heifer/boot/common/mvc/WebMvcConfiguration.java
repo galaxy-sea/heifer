35edca16fbe33b60f0b85db752b91ee41bb5c869
galaxy <469753862@qq.com>
2021-05-07 10:34:44 +0800

    修改

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/WebMvcConfiguration.java
@@ -6,9 +6,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import plus.wcj.heifer.boot.common.mvc.resolver.tenant.TenantMethodArgumentResolver;
-// import plus.wcj.heifer.boot.common.mvc.restful.ApiVersionRequestMappingHandlerMapping;
 
 import java.util.List;
 
