c6a293a355e9093e7e647227e89e3f83c1ad0058
weichangjin <weichangjin1996@gmail.com>
2021-05-23 23:40:42 +0800

    修改配置类

--- heifer-boot/src/test/java/plus/wcj/heifer/boot/dao/UserMapperTest.java
+++ heifer-boot/src/test/java/plus/wcj/heifer/boot/dao/UserMapperTest.java
@@ -1,37 +1,34 @@
-// package plus.wcj.heifer.boot.mapper;
-//
-// import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import plus.wcj.heifer.boot.entity.User;
-// import plus.wcj.heifer.boot.mapper.rbac.UserMapper;
-//
-// import java.util.List;
-//
-// @SpringBootTest
-// public class UserMapperTest {
-//
-//     @Autowired
-//     private UserMapper userMapper;
-//
-//
-//     @Test
-//     public void MybatisPlusTest() {
-//         List<User> users = userMapper.selectList(null);
-//         System.out.println(users);
-//     }
-//
-//
-//     @Test
-//     public void pageTest() {
-//         Page<User> userPage = userMapper.selectPage(new Page<User>(1, 20), null);
-//         System.out.println(userPage.getRecords());
-//     }
-//
-//     @Test
-//     public void xmlPageTest() {
-//         Page<User> userPage = userMapper.selectBy(new Page<User>(1, 20));
-//         System.out.println(userPage.getRecords());
-//     }
-// }
+package plus.wcj.heifer.boot.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import plus.wcj.heifer.boot.HeiferBootApplication;
+import plus.wcj.heifer.boot.entity.rbac.RbacUserDo;
+import plus.wcj.heifer.boot.service.rbac.RbacUserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(classes = HeiferBootApplication.class)
+public class UserMapperTest {
+
+    @Autowired
+    private RbacUserService userService;
+
+
+    @Test
+    public void MybatisPlusTest() {
+
+        List<RbacUserDo> rbacUserDoList = new ArrayList() {{
+            add(new RbacUserDo().setTenantOrgId(1L).setRbacDeptId(1L).setUsername(String.valueOf(Math.random() * 1000)).setPhone(String.valueOf(Math.random() * 1000)).setEmail(String.valueOf(Math.random() * 1000)).setPassword(String.valueOf(Math.random() * 1000)).setNickname(String.valueOf(Math.random() * 1000)).setIsAccountNonExpired(0).setIsAccountNonLocked(0).setIsCredentialsNonExpired(0).setIsEnabled(0));
+            add(new RbacUserDo().setTenantOrgId(1L).setRbacDeptId(1L).setUsername(String.valueOf(Math.random() * 1000)).setPhone(String.valueOf(Math.random() * 1000)).setEmail(String.valueOf(Math.random() * 1000)).setPassword(String.valueOf(Math.random() * 1000)).setNickname(String.valueOf(Math.random() * 1000)).setIsAccountNonExpired(0).setIsAccountNonLocked(0).setIsCredentialsNonExpired(0).setIsEnabled(0));
+            add(new RbacUserDo().setTenantOrgId(1L).setRbacDeptId(1L).setUsername(String.valueOf(Math.random() * 1000)).setPhone(String.valueOf(Math.random() * 1000)).setEmail(String.valueOf(Math.random() * 1000)).setPassword(String.valueOf(Math.random() * 1000)).setNickname(String.valueOf(Math.random() * 1000)).setIsAccountNonExpired(0).setIsAccountNonLocked(0).setIsCredentialsNonExpired(0).setIsEnabled(0));
+            add(new RbacUserDo().setTenantOrgId(1L).setRbacDeptId(1L).setUsername(String.valueOf(Math.random() * 1000)).setPhone(String.valueOf(Math.random() * 1000)).setEmail(String.valueOf(Math.random() * 1000)).setPassword(String.valueOf(Math.random() * 1000)).setNickname(String.valueOf(Math.random() * 1000)).setIsAccountNonExpired(0).setIsAccountNonLocked(0).setIsCredentialsNonExpired(0).setIsEnabled(0));
+            add(new RbacUserDo().setTenantOrgId(1L).setRbacDeptId(1L).setUsername(String.valueOf(Math.random() * 1000)).setPhone(String.valueOf(Math.random() * 1000)).setEmail(String.valueOf(Math.random() * 1000)).setPassword(String.valueOf(Math.random() * 1000)).setNickname(String.valueOf(Math.random() * 1000)).setIsAccountNonExpired(0).setIsAccountNonLocked(0).setIsCredentialsNonExpired(0).setIsEnabled(0));
+        }};
+        userService.saveBatch(rbacUserDoList);
+
+    }
+
+}
