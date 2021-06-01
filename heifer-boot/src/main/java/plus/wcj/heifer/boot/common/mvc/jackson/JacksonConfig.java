e146bba1028910e2fb010725a57dc63666c7afa7
weichangjin <weichangjin1996@gmail.com>
2021-06-01 23:49:12 +0800

    优化

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/jackson/JacksonConfig.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/common/mvc/jackson/JacksonConfig.java
@@ -18,12 +18,15 @@ public class JacksonConfig {
 
         // 数字不转科学计算法
         objectMapper.configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
+        // 枚举类型
+        // objectMapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
 
         // long转换成string
         SimpleModule simpleModule = new SimpleModule();
+        objectMapper.registerModule(simpleModule);
+
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        objectMapper.registerModule(simpleModule);
 
         return objectMapper;
     }
