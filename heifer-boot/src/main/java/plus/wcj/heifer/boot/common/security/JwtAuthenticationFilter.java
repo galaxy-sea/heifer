6da7b9df1d1c962780f78c9c3ff1ceb9f32c2367
galaxy <469753862@qq.com>
2021-06-11 09:51:22 +0800

    优化拦截器

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/common/security/JwtAuthenticationFilter.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/common/security/JwtAuthenticationFilter.java
@@ -2,20 +2,15 @@ package plus.wcj.heifer.boot.common.security;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import plus.wcj.heifer.boot.common.exception.ResultException;
-import plus.wcj.heifer.boot.common.exception.ResultStatus;
 import plus.wcj.heifer.boot.common.security.userdetails.CustomUserDetailsService;
 
 import javax.servlet.FilterChain;
@@ -23,9 +18,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
 
 /**
  * <p>
@@ -52,89 +44,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = jwtUtil.getJwtFromRequest(request);
 
-        if (checkIgnores(request)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+            if (StringUtils.isNotBlank(jwt)) {
 
-        String jwt = jwtUtil.getJwtFromRequest(request);
-
-        if (StringUtils.isNotBlank(jwt)) {
-            try {
                 String username = jwtUtil.getUsernameFromJWT(jwt);
 
                 UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                filterChain.doFilter(request, response);
-            } catch (Exception e) {
-                handlerExceptionResolver.resolveException(request, response, null, e);
+
             }
-        } else {
-            handlerExceptionResolver.resolveException(request, response, null, new ResultException(ResultStatus.UNAUTHORIZED));
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
         }
     }
-
-    /**
-     * 请求是否不需要进行权限拦截
-     *
-     * @param request 当前请求
-     * @return true - 忽略，false - 不忽略
-     */
-    private boolean checkIgnores(HttpServletRequest request) {
-        String method = request.getMethod();
-
-        HttpMethod httpMethod = HttpMethod.resolve(method);
-        if (Objects.isNull(httpMethod)) {
-            httpMethod = HttpMethod.GET;
-        }
-
-        Set<String> ignores = new HashSet<>();
-
-        switch (httpMethod) {
-            case GET:
-                ignores.addAll(customConfig.getIgnores().getGet());
-                break;
-            case PUT:
-                ignores.addAll(customConfig.getIgnores().getPut());
-                break;
-            case HEAD:
-                ignores.addAll(customConfig.getIgnores().getHead());
-                break;
-            case POST:
-                ignores.addAll(customConfig.getIgnores().getPost());
-                break;
-            case PATCH:
-                ignores.addAll(customConfig.getIgnores().getPatch());
-                break;
-            case TRACE:
-                ignores.addAll(customConfig.getIgnores().getTrace());
-                break;
-            case DELETE:
-                ignores.addAll(customConfig.getIgnores().getDelete());
-                break;
-            case OPTIONS:
-                ignores.addAll(customConfig.getIgnores().getOptions());
-                break;
-            default:
-                break;
-        }
-
-        ignores.addAll(customConfig.getIgnores().getPattern());
-
-        if (CollectionUtils.isNotEmpty(ignores)) {
-            for (String ignore : ignores) {
-                AntPathRequestMatcher matcher = new AntPathRequestMatcher(ignore, method);
-                if (matcher.matches(request)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
 }
