901fdd3ad083edbc2cb77a6b91afeb2af6853166
galaxy <469753862@qq.com>
2021-06-16 19:51:59 +0800

    准备修改jwt

--- heifer-boot/src/test/java/plus/wcj/heifer/boot/JwtTest.java
+++ heifer-boot/src/test/java/plus/wcj/heifer/boot/JwtTest.java
@@ -8,7 +8,14 @@ import com.nimbusds.jose.JWSObject;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.util.DateUtils;
 import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Date;
 
 /**
  * @author changjin wei(魏昌进)
@@ -35,4 +42,51 @@ public class JwtTest {
         System.out.println(serialize);
     }
 
+
+    @Test
+    public void test1() throws JOSEException {
+
+        //创建JWS头，设置签名算法和类型
+        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.HS256)
+                .type(JOSEObjectType.JWT)
+                .keyID("hahahahah")
+                .build();
+
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .issuer("iss")
+                .subject("sub")
+                .audience("aud")
+                .expirationTime(new Date(System.currentTimeMillis() + 10000000))
+                .notBeforeTime(new Date())
+                .issueTime(new Date())
+                .jwtID("jti")
+                .claim("my", "my")
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(jwsHeader, claimsSet);
+
+        signedJWT.sign(new MACSigner("12345678901234567890123456798012"));
+
+        System.out.println(signedJWT.serialize());
+    }
+
+
+
+    @Test
+    public void test2() throws JOSEException, ParseException {
+        // eyJraWQiOiJoYWhhaGFoYWgiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdWIiLCJhdWQiOiJhdWQiLCJuYmYiOjE2MjM4MjE0OTUsImlzcyI6ImlzcyIsImV4cCI6MTYyMzgzMTQ5NSwibXkiOiJteSIsImlhdCI6MTYyMzgyMTQ5NSwianRpIjoianRpIn0.ExX32qEBlx6NWcafO4uOh7alxyQR7mFJPAJMaQ-2BYY
+
+        SignedJWT jwt = SignedJWT.parse("eyJraWQiOiJoYWhhaGFoYWgiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdWIiLCJhdWQiOiJhdWQiLCJuYmYiOjE2MjM4MjM4MjYsImlzcyI6ImlzcyIsImV4cCI6MTYyMzgzMzgyNiwibXkiOiJteSIsImlhdCI6MTYyMzgyMzgyNiwianRpIjoianRpIn0.rU7rHa0p8iwyfk2RN8y5gQkzRJ1kKXFwaK1gR8YMHsI");
+
+        MACVerifier verifier = new MACVerifier("12345678901234567890123456798012");
+        //校验是否有效
+
+        System.out.println(jwt.verify(verifier));
+        // //校验超时
+        Date exp = jwt.getJWTClaimsSet().getExpirationTime();
+        System.out.println(DateUtils.isBefore(exp, new Date(), 60));
+
+
+
+    }
 }
