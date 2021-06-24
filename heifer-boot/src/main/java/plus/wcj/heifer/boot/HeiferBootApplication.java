496df1ba8410026e0850efd8e53d06f77444ced1
galaxy <469753862@qq.com>
2021-06-24 16:37:40 +0800

    优化配置

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/HeiferBootApplication.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/HeiferBootApplication.java
@@ -2,7 +2,6 @@ package plus.wcj.heifer.boot;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
@@ -10,10 +9,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @EnableOpenApi
 @SpringBootApplication
 @EnableEncryptableProperties
-@MapperScans({
-        @MapperScan("plus.wcj.heifer.boot.dao.**"),
-        @MapperScan("plus.wcj.heifer.boot.common.security.userdetails.dao")
-})
+@MapperScan({"plus.wcj.heifer.boot.dao.**", "plus.wcj.heifer.boot.common.security.userdetails.dao"})
 public class HeiferBootApplication {
 
     public static void main(String[] args) {
