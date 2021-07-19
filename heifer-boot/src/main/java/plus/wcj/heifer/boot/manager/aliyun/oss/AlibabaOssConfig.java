1d90e1439c584409a3726a3ed5dc1b460360d874
galaxy <469753862@qq.com>
2021-07-19 17:36:01 +0800

    balabalade

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/aliyun/oss/AlibabaOssConfig.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/aliyun/oss/AlibabaOssConfig.java
@@ -8,22 +8,27 @@ import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
+ * @see <a href="https://help.aliyun.com/document_detail/31927.htm?spm=a2c4g.11186623.2.9.17487d9cnTKLr7#concept-qp2-g4y-5db">服务端签名直传并设置上传回调 </a>
+ * @see <a href="https://help.aliyun.com/document_detail/91868.html?spm=a2c4g.11186623.6.1751.1b707a74R0LCkS">服务端签名直传并设置上传回调 > Java </a>
+ *
  * @author changjin wei(魏昌进)
  * @since 2021/7/16
+ *
  */
 public class AlibabaOssConfig {
 
     public static void main(String[] args) throws ParseException, FileNotFoundException {
-        String accessId = "<yourAccessKeyId>"; // 请填写您的AccessKeyId。
-        String accessKey = "<yourAccessKeySecret>"; // 请填写您的AccessKeySecret。
+        String accessId = "LTAI5tFHbYfReKAUyNHEeZG7"; // 请填写您的AccessKeyId。
+        String accessKey = "C9HtKXOPGhXGY3GIlnVnBMBDfHUIb2"; // 请填写您的AccessKeySecret。
         String endpoint = "oss-cn-hangzhou.aliyuncs.com"; // 请填写您的 endpoint。
-        String bucket = "bucket-name"; // 请填写您的 bucketname 。
+        String bucket = "heifer.oss-cn-hangzhou.aliyuncs.com"; // 请填写您的 bucketname 。
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
         String callbackUrl = "http://88.88.88.88:8888";
@@ -41,7 +46,7 @@ public class AlibabaOssConfig {
             policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
 
             String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
-            byte[] binaryData = postPolicy.getBytes("utf-8");
+            byte[] binaryData = postPolicy.getBytes(StandardCharsets.UTF_8);
             String encodedPolicy = BinaryUtil.toBase64String(binaryData);
             String postSignature = ossClient.calculatePostSignature(postPolicy);
 
@@ -54,19 +59,12 @@ public class AlibabaOssConfig {
             respMap.put("expire", String.valueOf(expireEndTime / 1000));
             // respMap.put("expire", formatISO8601Date(expiration));
 
-            JSONObject jasonCallback = new JSONObject();
-            jasonCallback.put("callbackUrl", callbackUrl);
-            jasonCallback.put("callbackBody",
-                              "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}");
-            jasonCallback.put("callbackBodyType", "application/x-www-form-urlencoded");
-            String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
-            respMap.put("callback", base64CallbackBody);
 
-            JSONObject ja1 = JSONObject.fromObject(respMap);
-            // System.out.println(ja1.toString());
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST");
-            response(request, response, ja1.toString());
+            // JSONObject ja1 = JSONObject.fromObject(respMap);
+            // // System.out.println(ja1.toString());
+            // response.setHeader("Access-Control-Allow-Origin", "*");
+            // response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+            // response(request, response, ja1.toString());
 
         } catch (Exception e) {
             // Assert.fail(e.getMessage());
