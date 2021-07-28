1c13d83015c13493d62e6e0d8b6b161b9a5da4c2
galaxy <469753862@qq.com>
2021-07-28 17:32:09 +0800

    mongodb

---
+++ heifer-boot/src/test/java/plus/wcj/heifer/boot/manager/mongodb/mongodbTest.java
@@ -0,0 +1,41 @@
+package plus.wcj.heifer.boot.manager.mongodb;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+/**
+ * @author changjin wei(魏昌进)
+ * @since 2021/7/28
+ */
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class mongodbTest {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Test
+    public void testD() {
+        Demo demo = new Demo();
+        demo.setId(1112);
+        demo.setName("xiapweio");
+
+        Demo insert = mongoTemplate.insert(demo);
+
+
+    }
+
+
+    @Data
+    class Demo {
+        @MongoId
+        private Integer id;
+        private String name;
+    }
+
+
+}
