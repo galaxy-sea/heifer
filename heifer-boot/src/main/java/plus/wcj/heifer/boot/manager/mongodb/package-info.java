b98adc9f20e24bfb133bb92d57cabadf36703962
weichangjin <weichangjin1996@gmail.com>
2021-08-01 23:27:17 +0800

    date,time utils

---
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/mongodb/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @author  changjinwei
+ * @since  2021/8/1
+ */package plus.wcj.heifer.boot.manager.mongodb;
---
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/utils/DateUtils.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *  <a href="https://github.com/xkzhangsan/xk-time">xk-time</a>
+ *
+ * @see com.xkzhangsan.time.converter.DateTimeConverterUtil
+ * @see com.xkzhangsan.time.calculator.DateTimeCalculatorUtil   日期计算工具类
+ * @see com.xkzhangsan.time.formatter.DateTimeFormatterUtil     日期格式化和解析工具类
+ * @see com.xkzhangsan.time.calendar.CalendarUtil               日历工具类
+ * @see com.xkzhangsan.time.LunarDate                           农历日期类
+ * @see com.xkzhangsan.time.holiday.HolidayUtil                 节假日计算工具类
+ * @see com.xkzhangsan.time.cron.CronExpressionUtil             Cron表达式工具类
+ * @see com.xkzhangsan.time.cost.CostUtil                       计算耗时工具
+ * @see com.xkzhangsan.time.nlp.TimeNLPUtil                     时间自然语言分析工具类
+ * @see com.xkzhangsan.time.constants.XkTimeConstant            时间单位常量类
+ *
+ * @author changjinwei
+ * @since 2021/8/1
+ */
+package plus.wcj.heifer.boot.utils;
