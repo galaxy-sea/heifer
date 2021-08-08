63470091201b3f14a2f83c16f0f55da506ebd80e
weichangjin <weichangjin1996@gmail.com>
2021-08-08 21:37:46 +0800

    maven update

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/mongodb/package-info.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/mongodb/package-info.java
@@ -1,4 +1,6 @@
 /**
- * @author  changjinwei
- * @since  2021/8/1
- */package plus.wcj.heifer.boot.manager.mongodb;
+ * @see org.springframework.data.mongodb.core.MongoTemplate
+ * @author changjinwei
+ * @since 2021/8/1
+ */
+package plus.wcj.heifer.boot.manager.mongodb;
--- heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/package-info.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/package-info.java
@@ -1,4 +1,9 @@
 /**
- * @author  changjinwei
- * @since  2021/8/1
- */package plus.wcj.heifer.boot.manager.redis;
+ *
+ * @see org.springframework.data.redis.core.RedisTemplate
+ *
+ *
+ * @author changjinwei
+ * @since 2021/8/1
+ */
+package plus.wcj.heifer.boot.manager.redis;
