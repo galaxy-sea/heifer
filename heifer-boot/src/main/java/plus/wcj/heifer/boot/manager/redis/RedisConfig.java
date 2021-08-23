deda2e7111480585778953ca298603ed18fcb4ac
galaxy <469753862@qq.com>
2021-08-23 10:50:02 +0800

    回滚

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/RedisConfig.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * @author changjin wei(魏昌进)
@@ -15,11 +15,8 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<?, ?> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        template.setDefaultSerializer(RedisSerializer.json());
-        template.setKeySerializer(RedisSerializer.string());
-        template.afterPropertiesSet();
+        RedisTemplate<?, ?> template = new StringRedisTemplate(redisConnectionFactory);
+        template.setEnableTransactionSupport(false);
         return template;
     }
 }
