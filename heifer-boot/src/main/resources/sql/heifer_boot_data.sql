339da8ca1ce840f4a059de2f25637a1a6ecf9ce7
galaxy <469753862@qq.com>
2021-08-23 18:09:56 +0800

    sql 初始化

--- heifer-boot/src/main/resources/sql/heifer_boot_data.sql
+++ heifer-boot/src/main/resources/sql/heifer_boot_data.sql
@@ -1,95 +1,111 @@
-# 基础表信息
-TRUNCATE rbac_permission;
-INSERT INTO rbac_permission (id, parent_id, name, permission, type, sort, create_time, create_by, update_time, update_by)
-VALUES (1, 0, 'heifer管理端', 'heifer', 'CLIENT', 0, NULL, 1, NULL, NULL),
-       (2, 0, '测试', 'page:test', 'BUTTON', 1, NULL, 1, NULL, NULL),
-       (3, 0, '测试角色', 'page:test:role', 'BUTTON', 1, NULL, 1, NULL, NULL),
-       (4, 0, '测试用户', 'page:test:user', 'BUTTON', 1, NULL, 1, NULL, NULL)
-;
+# 组织架构
+TRUNCATE heifer_boot.rbac_org;
+INSERT INTO heifer_boot.rbac_org (id, name, create_time, create_by, update_time, update_by)
+VALUES (10, 'heifer后台机构', '2021-08-23 17:25:08', 1, '2021-08-23 17:25:59', NULL),
+       (20, 'heifer前台机构', '2021-08-23 17:25:59', 1, NULL, NULL);
 
-TRUNCATE rbac_org;
-INSERT INTO rbac_org (id, name, create_time, create_by, update_time, update_by)
-VALUES (1, 'heifer管理端', NULL, 1, NULL, NULL),
-       (2, 'heifer客户端', NULL, 1, NULL, NULL);
+# 部门
+TRUNCATE heifer_boot.rbac_dept;
+INSERT INTO heifer_boot.rbac_dept (id, parent_id, rbac_org_id, name, create_time, create_by, update_time, update_by)
+VALUES (10, 0, 10, 'heifer技术部', '2021-08-23 17:28:55', 1, NULL, NULL),
+       (11, 10, 10, 'heifer菜鸟组', '2021-08-23 17:28:55', 1, NULL, NULL),
+       (12, 10, 10, 'heifer老油条组', '2021-08-23 17:30:29', 1, NULL, NULL),
+       (20, 0, 10, 'heifer运营部', '2021-08-23 17:28:55', 1, NULL, NULL);
+
+# 角色
+TRUNCATE heifer_boot.rbac_role;
+INSERT INTO heifer_boot.rbac_role (id, rbac_org_id, name, create_time, create_by, update_time, update_by)
+VALUES (10, 10, 'heifer admin角色', '2021-08-23 17:33:32', 1, NULL, NULL),
+       (20, 10, 'heifer后端角色', '2021-08-23 17:33:32', 1, NULL, NULL),
+       (30, 10, 'heifer全栈角色', '2021-08-23 17:33:32', 1, NULL, NULL),
+       (40, 10, 'heifer测试角色', '2021-08-23 17:33:33', 1, NULL, NULL),
+       (50, 10, 'heifer产品角色', '2021-08-23 17:33:33', 1, NULL, NULL);
+
+# 用户
+TRUNCATE heifer_boot.rbac_user;
+INSERT INTO heifer_boot.rbac_user (id, rbac_org_id, username, phone, email, password, nickname, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled, create_time, create_by, update_time, update_by)
+VALUES (10, 10, 'admin', '12345678910', 'a@qq.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '管理员', TRUE, TRUE, TRUE, TRUE, '2021-08-23 17:36:51', 1, NULL, NULL),
+       (20, 10, 'it', '12345678910', 'a@qq.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '管理员', TRUE, TRUE, TRUE, TRUE, '2021-08-23 17:36:51', 1, NULL, NULL),
+       (30, 10, 'test', '12345678910', 'a@qq.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '管理员', TRUE, TRUE, TRUE, TRUE, '2021-08-23 17:36:51', 1, NULL, NULL);
+
+# 权限
+TRUNCATE heifer_boot.rbac_permission;
+INSERT INTO heifer_boot.rbac_permission (id, parent_id, name, permission, type, sort, create_time, create_by, update_time, update_by)
+VALUES (10, 0, 'heifer管理端', 'heifer', 'CLIENT', 0, NULL, 1, '2021-08-23 17:41:02', NULL),
+       (11, 10, '测试', 'heifer:test', 'BUTTON', 1, NULL, 1, '2021-08-23 17:41:02', NULL),
+       (12, 10, '测试角色', 'heifer:test:role', 'BUTTON', 1, NULL, 1, '2021-08-23 17:41:02', NULL),
+       (13, 10, '测试用户', 'heifer:test:user', 'BUTTON', 1, NULL, 1, '2021-08-23 17:41:02', NULL);
+
+# 用户——管理员
+TRUNCATE heifer_boot.rbac_admin;
+INSERT INTO heifer_boot.rbac_admin (id, rbac_user_id, rbac_dept_id, create_time, create_by, update_time, update_by)
+VALUES (10, 10, 10, '2021-08-23 17:44:32', 1, NULL, NULL),
+       (20, 20, 10, '2021-08-23 17:44:32', 1, NULL, NULL);
+
+# 用户-会员
+TRUNCATE heifer_boot.rbac_customer;
+INSERT INTO heifer_boot.rbac_customer (id, rbac_user_id, create_time, create_by, update_time, update_by)
+VALUES (10, 10, '2021-08-23 17:45:37', 1, NULL, NULL);
+
+# 机构-权限
+TRUNCATE heifer_boot.rbac_org_authority;
+INSERT INTO heifer_boot.rbac_org_authority (id, rbac_org_id, rbac_permission_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:47:50', 1),
+       (11, 10, 11, '2021-08-23 17:47:50', 1),
+       (12, 10, 12, '2021-08-23 17:47:50', 1),
+       (13, 10, 13, '2021-08-23 17:47:50', 1);
+
+# 角色-功能权限
+TRUNCATE heifer_boot.rbac_role_authority;
+INSERT INTO heifer_boot.rbac_role_authority (id, rbac_role_id, rbac_permission_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:49:55', 1),
+       (11, 10, 11, '2021-08-23 17:49:55', 1),
+       (12, 10, 12, '2021-08-23 17:49:55', 1),
+       (13, 10, 13, '2021-08-23 17:49:55', 1);
+
+# 角色-数据权限
+TRUNCATE heifer_boot.rbac_role_data_power;
+INSERT INTO heifer_boot.rbac_role_data_power (id, rbac_role_id, rbac_dept_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:51:22', 1),
+       (11, 10, 11, '2021-08-23 17:51:22', 1),
+       (12, 10, 12, '2021-08-23 17:51:22', 1),
+       (13, 10, 20, '2021-08-23 17:51:22', 1);
+
+### 用户-功能权限
+TRUNCATE heifer_boot.rbac_user_authority;
+INSERT INTO heifer_boot.rbac_user_authority (id, rbac_user_id, rbac_permission_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:54:20', 1),
+       (11, 10, 11, '2021-08-23 17:54:20', 1),
+       (12, 10, 12, '2021-08-23 17:54:20', 1),
+       (13, 10, 13, '2021-08-23 17:54:20', 1);
+# 用户-数据权限
+TRUNCATE heifer_boot.rbac_user_data_power;
+INSERT INTO heifer_boot.rbac_user_data_power (id, rbac_user_id, rbac_dept_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:56:08', 1),
+       (11, 10, 11, '2021-08-23 17:56:08', 1),
+       (12, 10, 12, '2021-08-23 17:56:08', 1),
+       (13, 10, 20, '2021-08-23 17:56:08', 1);
+
+# 用户-all 数据权限和功能权限
+TRUNCATE heifer_boot.rbac_user_manage;
+INSERT INTO heifer_boot.rbac_user_manage (id, rbac_user_id, is_all_power, is_all_authority, create_time, create_by, update_time, update_by)
+VALUES (10, 10, TRUE, TRUE, '2021-08-23 17:57:11', 1, NULL, NULL);
+
+# 用户-拥有的角色
+TRUNCATE heifer_boot.rbac_user_role_rel;
+INSERT INTO heifer_boot.rbac_user_role_rel (id, rbac_user_id, rbac_role_id, create_time, create_by)
+VALUES (10, 10, 10, '2021-08-23 17:58:15', 1),
+       (11, 10, 20, '2021-08-23 17:58:15', 1),
+       (12, 10, 30, '2021-08-23 17:58:15', 1),
+       (13, 10, 40, '2021-08-23 17:58:15', 1),
+       (14, 10, 50, '2021-08-23 17:58:15', 1);
 
 
-TRUNCATE rbac_dept;
-INSERT INTO rbac_dept (id, parent_id, rbac_org_id, name, create_time, create_by, update_time, update_by)
-VALUES (1, 0, 1, 'heifer管理端的部门1', NULL, 1, NULL, NULL),
-       (2, 1, 1, 'heifer管理端的部门2', NULL, 1, NULL, NULL),
-       (10, 0, 2, 'heifer客户端的部门1', NULL, 1, NULL, NULL),
-       (11, 10, 2, 'heifer客户端的部门1', NULL, 1, NULL, NULL);
 
-
-TRUNCATE rbac_user;
-INSERT INTO rbac_user (id, rbac_org_id, username, phone, email, password, nickname, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled, create_time, create_by, update_time, update_by)
-VALUES (1, 1, 'admin', '12345678910', 'a@a.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '管理员', TRUE, TRUE, TRUE, TRUE, NULL, 1, NULL, NULL),
-       (2, 1, 'user', '12345678910', 'a@a.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '普通用户', TRUE, TRUE, TRUE, TRUE, NULL, 1, NULL, NULL),
-       (3, 1, 'notRole', '12345678910', 'a@a.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '普通用户', TRUE, TRUE, TRUE, TRUE, NULL, 1, NULL, NULL),
-       (10, 2, 'admin', '12345678910', 'a@a.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '管理员', TRUE, TRUE, TRUE, TRUE, NULL, 1, NULL, NULL),
-       (11, 2, 'user', '12345678910', 'a@a.com', '$2a$10$64iuSLkKNhpTN19jGHs7xePvFsub7ZCcCmBqEYw8fbACGTE3XetYq', '普通用户', TRUE, TRUE, TRUE, TRUE, NULL, 1, NULL, NULL);
-
-TRUNCATE rbac_role;
-INSERT INTO rbac_role (id, rbac_org_id, name, create_time, create_by, update_time, update_by)
-VALUES (1, 1, 'heifer管理端admin角色', '2021-07-01 10:07:13', 1, NULL, NULL),
-       (2, 1, 'heifer管理端user角色', '2021-07-01 10:07:13', 1, NULL, NULL),
-       (10, 2, 'heifer客户端admin角色', '2021-07-01 10:07:13', 1, NULL, NULL),
-       (11, 2, 'heifer客户端user角色', '2021-07-01 10:07:13', 1, NULL, NULL);
-
-# 功能权限相关
-TRUNCATE rbac_org_authority;
-INSERT INTO rbac_org_authority(id, rbac_org_id, rbac_permission_id, create_time, create_by)
-VALUES (1, 1, 1, NULL, 1),
-       (1, 1, 2, NULL, 1),
-       (1, 1, 3, NULL, 1),
-       (1, 1, 4, NULL, 1);
-
-TRUNCATE rbac_role_authority;
-INSERT INTO rbac_role_authority(id, rbac_role_id, rbac_permission_id, create_time, create_by)
-VALUES (1, 1, 1, NULL, 1),
-       (1, 1, 2, NULL, 1),
-       (1, 1, 3, NULL, 1),
-       (1, 1, 4, NULL, 1);
-
-TRUNCATE rbac_user_authority;
-INSERT INTO rbac_user_authority(id, rbac_user_id, rbac_permission_id, create_time, create_by)
-VALUES (1, 3, 1, NULL, 1),
-       (1, 3, 4, NULL, 1);
-
-
-# 数据权限相关
-TRUNCATE rbac_role_data_power;
-INSERT INTO rbac_role_data_power(id, rbac_role_id, rbac_dept_id, create_time, create_by)
-VALUES (1, 1, 1, NULL, 1);
-
-
-TRUNCATE rbac_user_data_power;
-INSERT INTO rbac_user_data_power(id, rbac_user_id, rbac_dept_id, create_time, create_by)
-VALUES (1, 1, 2, NULL, 1);
-
-
-# 用户相关
-TRUNCATE rbac_admin;
-INSERT INTO rbac_admin(id, rbac_user_id, rbac_dept_id, create_time, create_by, update_time, update_by)
-VALUES (1, 1, 1, NULL, 1, NULL, NULL);
-
-TRUNCATE rbac_customer;
-INSERT INTO rbac_customer(id, rbac_user_id, create_time, create_by, update_time, update_by)
-VALUES (1, 1, NULL, 1, NULL, NULL),
-       (1, 2, NULL, 1, NULL, NULL),
-       (1, 3, NULL, 1, NULL, NULL);
-
-TRUNCATE rbac_user_role_rel;
-INSERT INTO rbac_user_role_rel(id, rbac_user_id, rbac_role_id, create_time, create_by)
-VALUES (1, 1, 1, NULL, 1),
-       (1, 2, 2, NULL, 1);
-
-TRUNCATE rbac_user_manage;
-INSERT INTO rbac_user_manage(id, rbac_user_id, is_all_power, is_all_authority, create_time, create_by, update_time, update_by)
-VALUES (1, 1, TRUE, TRUE, NULL, 1, NULL, NULL);
-
-
-
+###
+###
+###
+###
 ALTER TABLE rbac_admin
     ADD CONSTRAINT a15 FOREIGN KEY (rbac_user_id) REFERENCES rbac_user (id);
 ALTER TABLE rbac_admin
