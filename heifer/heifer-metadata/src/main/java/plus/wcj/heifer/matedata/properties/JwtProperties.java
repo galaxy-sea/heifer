24db5c0f696f298d62e4caa623c5053f676a1512
weichangjin <469753862@qq.com>
2022-01-03 20:32:10 +0800

    修改错误表达式

--- heifer/heifer-metadata/src/main/java/plus/wcj/heifer/matedata/properties/JwtProperties.java
+++ heifer/heifer-metadata/src/main/java/plus/wcj/heifer/matedata/properties/JwtProperties.java
@@ -17,18 +17,18 @@ public class JwtProperties {
     /**
      * jwt 加密 key，默认值：xxxxxxxxxxxxxxx.
      */
-    @Value(value = "heifer.jwt.key?heifer.jwt.key:xxxxxxxxxxxxxxx")
+    @Value(value = "${heifer.jwt.key?heifer.jwt.key:xxxxxxxxxxxxxxx}")
     private String key;
 
     /**
      * jwt 过期时间，默认值：600000 {@code 10 分钟}.
      */
-    @Value(value = "heifer.jwt.ttl?heifer.jwt.ttl:600000L")
+    @Value(value = "${heifer.jwt.ttl?heifer.jwt.ttl:600000}")
     private Long ttl;
 
     /**
      * 开启 记住我 之后 jwt 过期时间，默认值 604800000 {@code 7 天}
      */
-    @Value(value = "heifer.jwt.remember?heifer.jwt.remember:604800000L")
+    @Value(value = "${heifer.jwt.remember?heifer.jwt.remember:604800000}")
     private Long remember;
 }
