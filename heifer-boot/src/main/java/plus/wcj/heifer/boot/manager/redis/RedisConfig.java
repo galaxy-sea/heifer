67ad7390bf92eee465be6a030efea65dce359167
galaxy <469753862@qq.com>
2022-01-07 11:08:22 +0800

    修改命名

--- heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/RedisConfig.java
+++ heifer-boot/src/main/java/plus/wcj/heifer/boot/manager/redis/RedisConfig.java
@@ -1,15 +1,12 @@
 package plus.wcj.heifer.boot.manager.redis;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -27,16 +24,23 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<?, ?> template = new StringRedisTemplate(redisConnectionFactory);
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(RedisSerializer.json());
         template.setEnableTransactionSupport(false);
+        template.afterPropertiesSet();
         return template;
     }
 
 
     @Bean
-    public CacheManager dynamicTtlCacheManager(RedisConnectionFactory factory) {
+    public RandomTtlRedisCacheManager randomTtlRedisCacheManager(RedisConnectionFactory factory) {
         RedisCacheWriter redisCacheWriter = RedisCacheWriter.lockingRedisCacheWriter(factory);
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        RedisCacheConfiguration config = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()));
         return new RandomTtlRedisCacheManager(redisCacheWriter, config, Duration.ofMinutes(10), Duration.ofMinutes(20));
     }
 
