b11e998cea9817e80b53f8b6159963757c060322
galaxy <469753862@qq.com>
2022-02-23 17:34:38 +0800

    优化

--- heifer/heifer-metadata/src/main/java/plus/wcj/heifer/matedata/service/UserPrincipalService.java
+++ heifer/heifer-metadata/src/main/java/plus/wcj/heifer/matedata/service/UserPrincipalService.java
@@ -1,6 +1,7 @@
 package plus.wcj.heifer.matedata.service;
 
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,8 +14,10 @@ import java.util.List;
  */
 public interface UserPrincipalService {
     @GetMapping("/account/getAllPermission")
+    @Cacheable(cacheNames = "permission", key = "#p0+':'+#p1")
     List<String> getAllPermission(@RequestParam Long accountId, @RequestParam Long tenantId);
 
     @GetMapping("/account/getAllPower")
+    @Cacheable(cacheNames = "power", key = "#p0+':'+#p1")
     List<?> getAllPower(@RequestParam Long accountId, @RequestParam Long tenantId);
 }
