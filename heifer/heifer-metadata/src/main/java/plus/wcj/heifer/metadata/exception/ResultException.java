8f6b45b8ae0025a7ecb6c5c0c89577ce3d4b537a
weichangjin <469753862@qq.com>
2022-05-05 15:52:24 +0800

    优化ResultException

--- heifer/heifer-metadata/src/main/java/plus/wcj/heifer/metadata/exception/ResultException.java
+++ heifer/heifer-metadata/src/main/java/plus/wcj/heifer/metadata/exception/ResultException.java
@@ -12,20 +12,14 @@ public class ResultException extends RuntimeException {
      * 业务异常信息信息
      */
     private final ResultStatus resultStatus;
-    private final String originalMessage;
     private final Object[] ages;
 
     public ResultException() {
         this(ResultStatusEnum.INTERNAL_SERVER_ERROR);
     }
 
-    public ResultException(ResultStatus resultStatus) {
-        this(resultStatus, null);
-    }
-
-    public ResultException(ResultStatus resultStatus, String originalMessage, String... ages) {
+    public ResultException(ResultStatus resultStatus, String... ages) {
         this.resultStatus = resultStatus;
-        this.originalMessage = originalMessage;
         this.ages = ages;
     }
 }
