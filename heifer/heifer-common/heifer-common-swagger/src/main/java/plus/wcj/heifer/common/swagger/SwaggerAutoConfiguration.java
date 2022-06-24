05266accf0059bb7de77590fb3f0ea097026af0d
galaxy <469753862@qq.com>
2022-06-24 15:02:17 +0800

    swagger使用增加默认配置

--- heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SwaggerAutoConfiguration.java
+++ heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SwaggerAutoConfiguration.java
@@ -32,6 +32,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -47,6 +48,7 @@ import java.util.stream.Collectors;
  * @since 2022/6/20
  */
 @Configuration
+@PropertySource("classpath:swagger.properties")
 public class SwaggerAutoConfiguration implements WebMvcConfigurer {
 
 
