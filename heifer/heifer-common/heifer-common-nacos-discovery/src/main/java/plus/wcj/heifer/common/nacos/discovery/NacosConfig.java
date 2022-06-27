f61488467c2702228d188a6d27f3afcd47465914
galaxy <469753862@qq.com>
2022-06-27 11:02:35 +0800

    nacos 增加 编译信息

--- heifer/heifer-common/heifer-common-nacos-discovery/src/main/java/plus/wcj/heifer/common/nacos/discovery/NacosConfig.java
+++ heifer/heifer-common/heifer-common-nacos-discovery/src/main/java/plus/wcj/heifer/common/nacos/discovery/NacosConfig.java
@@ -18,9 +18,12 @@ package plus.wcj.heifer.common.nacos.discovery;
 
 import com.alibaba.cloud.nacos.registry.NacosRegistrationCustomizer;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Map;
 import java.util.Properties;
@@ -33,7 +36,7 @@ import java.util.Properties;
 public class NacosConfig {
 
     @Bean
-    public NacosRegistrationCustomizer nacosRegistrationCustomizer() {
+    public NacosRegistrationCustomizer jdkMetadata() {
         return registration -> {
             Map<String, String> metadata = registration.getMetadata();
             Properties properties = System.getProperties();
@@ -60,6 +63,25 @@ public class NacosConfig {
             metadata.put("os.version", properties.getProperty("os.version"));
             // os架构
             metadata.put("os.arch", properties.getProperty("os.arch"));
+        };
+    }
+
+    /**
+     * see <a href="https://juejin.cn/post/6962294037122383886#heading-2">maven spring-boot-maven-plugin</a>
+     *
+     * @param buildProperties buildProperties info
+     *
+     * @return NacosRegistrationCustomizer
+     */
+    @Bean
+    @ConditionalOnBean(BuildProperties.class)
+    public NacosRegistrationCustomizer buildPropertiesMetadata(BuildProperties buildProperties) {
+        return registration -> {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            Map<String, String> metadata = registration.getMetadata();
+            metadata.put("project.version", buildProperties.getVersion());
+            metadata.put("project.buildTime", dateTimeFormatter.format(buildProperties.getTime()));
         };
     }
 }
