00eb3d0441bebeeb9a87a262a997ed8459ff8bef
小魏,小魏,我们要去哪里呀 <weichangjin1996@gmail.com>
2023-06-09 09:36:39 +0800

    swagger 适配

--- heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SecurityAnnotationOperationCustomizer.java
+++ heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SecurityAnnotationOperationCustomizer.java
@@ -24,9 +24,7 @@ import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreFilter;
-import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
-import plus.wcj.heifer.metadata.annotation.IgnoreWebSecurity;
 
 import java.util.Optional;
 
@@ -38,31 +36,60 @@ import static plus.wcj.heifer.common.swagger.HtmlTool.*;
  */
 @Order
 public class SecurityAnnotationOperationCustomizer implements GlobalOperationCustomizer {
+
     @Override
     public Operation customize(Operation operation, HandlerMethod handlerMethod) {
-        StringBuffer notes = new StringBuffer();
 
-        Optional.ofNullable(handlerMethod.getMethodAnnotation(PostAuthorize.class))
-                .or(() -> Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostAuthorize.class)))
-                .ifPresent(ann -> notes.append(b("PostAuthorize: ")).append(code(ann.value())));
-
-        Optional.ofNullable(handlerMethod.getMethodAnnotation(PostFilter.class))
-                .or(() -> Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostFilter.class)))
-                .ifPresent(ann -> notes.append(b("PostFilter: ")).append(code(ann.value())));
-
-        Optional.ofNullable(handlerMethod.getMethodAnnotation(PreAuthorize.class))
-                .or(() -> Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreAuthorize.class)))
-                .ifPresent(ann -> notes.append(b("PreAuthorize: ")).append(code(ann.value())));
-
-        Optional.ofNullable(handlerMethod.getMethodAnnotation(PreFilter.class))
-                .or(() -> Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreFilter.class)))
-                .ifPresent(ann -> notes.append(b("PreFilter: ")).append(code(ann.value())));
-
-        String description = operation.getDescription();
-        description = StringUtils.hasText(description) ? p(description, notes.toString()) : notes.toString();
-        operation.setDescription(description);
+        StringBuffer classNote = classSecurityAnnotation(handlerMethod);
+        StringBuffer methodNote = methodSecurityAnnotation(handlerMethod);
+        noteAppend("class: ", classNote, operation);
+        noteAppend("method: ", methodNote, operation);
 
         return operation;
+    }
+
+    private void noteAppend(String prefix, StringBuffer classNote, Operation operation) {
+        if (classNote.isEmpty()) {
+            return;
+        }
+        String note = prefix + classNote;
+        String description = operation.getDescription();
+        operation.setDescription(p(description, note));
+    }
+
+    private StringBuffer methodSecurityAnnotation(HandlerMethod handlerMethod) {
+        StringBuffer note = new StringBuffer();
+
+        Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostAuthorize.class))
+                .ifPresent(ann -> note.append(b("PostAuthorize: ")).append(code(ann.value())));
+
+        Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostFilter.class))
+                .ifPresent(ann -> note.append(b("PostFilter: ")).append(code(ann.value())));
+
+        Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreAuthorize.class))
+                .ifPresent(ann -> note.append(b("PreAuthorize: ")).append(code(ann.value())));
+
+        Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreFilter.class))
+                .ifPresent(ann -> note.append(b("PreFilter: ")).append(code(ann.value())));
+        return note;
+    }
+
+    private StringBuffer classSecurityAnnotation(HandlerMethod handlerMethod) {
+        StringBuffer note = new StringBuffer();
+
+        Optional.ofNullable(handlerMethod.getMethodAnnotation(PostAuthorize.class))
+                .ifPresent(ann -> note.append(b("PostAuthorize: ")).append(code(ann.value())));
+
+        Optional.ofNullable(handlerMethod.getMethodAnnotation(PostFilter.class))
+                .ifPresent(ann -> note.append(b("PostFilter: ")).append(code(ann.value())));
+
+        Optional.ofNullable(handlerMethod.getMethodAnnotation(PreAuthorize.class))
+                .ifPresent(ann -> note.append(b("PreAuthorize: ")).append(code(ann.value())));
+
+        Optional.ofNullable(handlerMethod.getMethodAnnotation(PreFilter.class))
+                .ifPresent(ann -> note.append(b("PreFilter: ")).append(code(ann.value())));
+
+        return note;
     }
 }
 
