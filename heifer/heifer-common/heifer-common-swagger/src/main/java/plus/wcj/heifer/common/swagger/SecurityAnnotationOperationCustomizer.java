b20d28fbe4150b8008703e6b71650b9d61ad43e0
小魏,小魏,我们要去哪里呀 <weichangjin1996@gmail.com>
2023-08-18 16:58:52 +0800

    swagger 优化Spring Security的注解显示

--- heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SecurityAnnotationOperationCustomizer.java
+++ heifer/heifer-common/heifer-common-swagger/src/main/java/plus/wcj/heifer/common/swagger/SecurityAnnotationOperationCustomizer.java
@@ -19,6 +19,7 @@ package plus.wcj.heifer.common.swagger;
 import io.swagger.v3.oas.models.Operation;
 import org.springdoc.core.customizers.GlobalOperationCustomizer;
 import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
@@ -26,9 +27,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.method.HandlerMethod;
 
+import java.lang.annotation.Annotation;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import static plus.wcj.heifer.common.swagger.HtmlTool.*;
+import static plus.wcj.heifer.common.swagger.HtmlTool.ALIZARIN;
+import static plus.wcj.heifer.common.swagger.HtmlTool.b;
+import static plus.wcj.heifer.common.swagger.HtmlTool.code;
+import static plus.wcj.heifer.common.swagger.HtmlTool.p;
 
 /**
  * @author changjin wei(魏昌进)
@@ -42,17 +49,17 @@ public class SecurityAnnotationOperationCustomizer implements GlobalOperationCus
 
         StringBuffer classNote = classSecurityAnnotation(handlerMethod);
         StringBuffer methodNote = methodSecurityAnnotation(handlerMethod);
-        noteAppend(b("class: ", ALIZARIN), classNote, operation);
-        noteAppend(b("method: ", ALIZARIN), methodNote, operation);
+        noteAppend(b("class( ", ALIZARIN), classNote, ")", operation);
+        noteAppend(b("method( ", ALIZARIN), methodNote, ")", operation);
 
         return operation;
     }
 
-    private void noteAppend(String prefix, StringBuffer classNote, Operation operation) {
+    private void noteAppend(String prefix, StringBuffer classNote, String suffix, Operation operation) {
         if (classNote.isEmpty()) {
             return;
         }
-        String note = prefix + classNote;
+        String note = prefix + classNote + suffix;
         String description = operation.getDescription();
         operation.setDescription(p(description, note));
     }
@@ -61,16 +68,16 @@ public class SecurityAnnotationOperationCustomizer implements GlobalOperationCus
         StringBuffer note = new StringBuffer();
 
         Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostAuthorize.class))
-                .ifPresent(ann -> note.append(b("PostAuthorize: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PostAuthorize: "));
 
         Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PostFilter.class))
-                .ifPresent(ann -> note.append(b("PostFilter: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PostFilter: "));
 
         Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreAuthorize.class))
-                .ifPresent(ann -> note.append(b("PreAuthorize: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PreAuthorize: "));
 
         Optional.ofNullable(AnnotatedElementUtils.getMergedAnnotation(handlerMethod.getBeanType(), PreFilter.class))
-                .ifPresent(ann -> note.append(b("PreFilter: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PreFilter: "));
         return note;
     }
 
@@ -78,18 +85,36 @@ public class SecurityAnnotationOperationCustomizer implements GlobalOperationCus
         StringBuffer note = new StringBuffer();
 
         Optional.ofNullable(handlerMethod.getMethodAnnotation(PostAuthorize.class))
-                .ifPresent(ann -> note.append(b("PostAuthorize: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PostAuthorize: "));
 
         Optional.ofNullable(handlerMethod.getMethodAnnotation(PostFilter.class))
-                .ifPresent(ann -> note.append(b("PostFilter: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PostFilter: "));
 
         Optional.ofNullable(handlerMethod.getMethodAnnotation(PreAuthorize.class))
-                .ifPresent(ann -> note.append(b("PreAuthorize: ")).append(code(ann.value())));
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PreAuthorize: "));
 
         Optional.ofNullable(handlerMethod.getMethodAnnotation(PreFilter.class))
-                .ifPresent(ann -> note.append(b("PreFilter: ")).append(code(ann.value())));
-
+                .ifPresent(ann -> appendAnnotationValue(ann, note, "PreFilter: "));
         return note;
     }
+
+
+    private void appendAnnotationValue(Annotation annotation, StringBuffer note, String text) {
+        note.append(b(text)).append(code(getAuthority(AnnotationUtils.getAnnotationAttributes(annotation).get("value"))));
+    }
+
+    private String getAuthority(Object value) {
+        String regex = "'(.*?)'";
+
+        String input = (String) value;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            return matcher.group(1);
+        }
+        return input;
+    }
+
 }
 
